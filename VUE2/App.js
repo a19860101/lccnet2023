c1f8692b704713f38960206161eeec92b1f76cb2
Zac <a19860101@gmail.com>
2023-07-28 19:39:09 +0800

    20230728

---
+++ VUE2/App.js
@@ -0,0 +1,14 @@
+import Btn from './components/Btn.js';
+export default {
+    data(){
+        return {
+            title: 'hello vue',
+            msg:'123'
+        }
+    },
+    components:{
+        'Btn':Btn
+    },
+    methods:{},
+    computed:{}
+}
