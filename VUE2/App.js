95b45c200b834a944275cbeb3a7e4e21461f7938
Zac <a19860101@gmail.com>
2023-07-28 19:55:25 +0800

    20230728

--- VUE2/App.js
+++ VUE2/App.js
@@ -7,7 +7,8 @@ export default {
         }
     },
     components:{
-        'Btn':Btn
+        // 'Btn':Btn
+        Btn
     },
     methods:{},
     computed:{}
---
+++ VUE2/components/Test.js
@@ -0,0 +1,9 @@
+const obj = {
+    name:'john',
+    mail: 'john@mail.com',
+}
+const user = {
+    name:'max',
+    mail:' max@gamil.com'
+}
+export {obj,user};
